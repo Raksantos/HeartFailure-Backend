b37400955067ad7d5b3db672b9533240fe041c00
Raksantos <rodrigosnt3@gmail.com>
2022-01-15 01:19:24 -0300

    Adding basic structure to work with the model

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
 from flask import Flask, request, jsonify
-from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import pickle
-
-
-data = [[40,
- 140,
- 289,
- 0,
- 172,
- 0,
- 0,
- 1,
- 0,
- 1,
- 0,
- 0,
- 0,
- 1,
- 0,
- 1,
- 0,
- 0,
- 0,
- 1,
- 0]]
-
-example = pd.DataFrame(data,
 
 columns=['Idade (anos)', 'Pressão sanguínea em repouso (mm Hg)',
        'Colesterol (mm/dl)', 'Açúcar no sangue em jejum',
@@ -39,15 +13,70 @@ columns=['Idade (anos)', 'Pressão sanguínea em repouso (mm Hg)',
        'Angina induzida por exercício_N', 'Angina induzida por exercício_Y',
        'Inclinação do segmento ST de pico do exercício_Down',
        'Inclinação do segmento ST de pico do exercício_Flat',
-       'Inclinação do segmento ST de pico do exercício_Up', 'Diagnóstico'])
+       'Inclinação do segmento ST de pico do exercício_Up']
 
+# data = [[40,
+#  140,
+#  289,
+#  0,
+#  172,
+#  0,
+#  0,
+#  1,
+#  0,
+#  1,
+#  0,
+#  0,
+#  0,
+#  1,
+#  0,
+#  1,
+#  0,
+#  0,
+#  0,
+#  1,
+#  0]]
 
+# example = pd.DataFrame(data,
+
+# columns=['Idade (anos)', 'Pressão sanguínea em repouso (mm Hg)',
+#        'Colesterol (mm/dl)', 'Açúcar no sangue em jejum',
+#        'Frequência cardíaca máxima alcançada', 'Oldpeak', 'Sexo_F', 'Sexo_M',
+#        'Tipo de dor no peito_ASY', 'Tipo de dor no peito_ATA',
+#        'Tipo de dor no peito_NAP', 'Tipo de dor no peito_TA',
+#        'Resultados de eletrocardiograma em repouso_LVH',
+#        'Resultados de eletrocardiograma em repouso_Normal',
+#        'Resultados de eletrocardiograma em repouso_ST',
+#        'Angina induzida por exercício_N', 'Angina induzida por exercício_Y',
+#        'Inclinação do segmento ST de pico do exercício_Down',
+#        'Inclinação do segmento ST de pico do exercício_Flat',
+#        'Inclinação do segmento ST de pico do exercício_Up', 'Diagnóstico'], columns = columns)
+
+# example = example.drop(['Diagnóstico'], axis=1)
+# X_valid = ro_scaler.transform(example)
 
 model = pickle.load(open('knn_model.sav', 'rb'))
 ro_scaler = pickle.load(open('ro_scaler.sav', 'rb'))
 
-example = example.drop(['Diagnóstico'], axis=1)
-X_valid = ro_scaler.transform(example)
+app = app = Flask(__name__)
 
-print(model.predict(X_valid))
+@app.route('/')
+def home():
+    return 'Data Science Project API'
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    data_input = [data[col] for col in columns]
+
+    df = pd.DataFrame(data=[data_input], columns=columns)
+
+    X_valid = ro_scaler.transform(df)
+
+    result = model.predict(X_valid)
+
+    result = result.tolist()
+
+    return jsonify(Result=result[0])
+
+app.run(debug=True)
