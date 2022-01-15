b3cddeb2bcf88b3fe49e71b4e75b20f9cb3a9976
Raksantos <rodrigosnt3@gmail.com>
2022-01-15 00:35:25 -0300

    Getting scaller to KNN and testing scaller

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
+from flask import Flask, request, jsonify
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 import pickle
 
-print('oi')
+
+data = [[40,
+ 140,
+ 289,
+ 0,
+ 172,
+ 0,
+ 0,
+ 1,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0]]
+
+example = pd.DataFrame(data,
+
+columns=['Idade (anos)', 'Pressão sanguínea em repouso (mm Hg)',
+       'Colesterol (mm/dl)', 'Açúcar no sangue em jejum',
+       'Frequência cardíaca máxima alcançada', 'Oldpeak', 'Sexo_F', 'Sexo_M',
+       'Tipo de dor no peito_ASY', 'Tipo de dor no peito_ATA',
+       'Tipo de dor no peito_NAP', 'Tipo de dor no peito_TA',
+       'Resultados de eletrocardiograma em repouso_LVH',
+       'Resultados de eletrocardiograma em repouso_Normal',
+       'Resultados de eletrocardiograma em repouso_ST',
+       'Angina induzida por exercício_N', 'Angina induzida por exercício_Y',
+       'Inclinação do segmento ST de pico do exercício_Down',
+       'Inclinação do segmento ST de pico do exercício_Flat',
+       'Inclinação do segmento ST de pico do exercício_Up', 'Diagnóstico'])
+
+
+
+model = pickle.load(open('knn_model.sav', 'rb'))
+ro_scaler = pickle.load(open('ro_scaler.sav', 'rb'))
+
+example = example.drop(['Diagnóstico'], axis=1)
+X_valid = ro_scaler.transform(example)
+
+print(model.predict(X_valid))
+
