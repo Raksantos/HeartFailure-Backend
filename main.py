f0dc3979754a5dd85d27cec8fd4b23106018f934
Raksantos <rodrigosnt3@gmail.com>
2022-01-16 02:25:25 -0300

    Minor fixes on requirements.txt and adding CORS

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import pickle
+from flask_cors import CORS
 
-columns=['Idade (anos)', 'Pressão sanguínea em repouso (mm Hg)',
-       'Colesterol (mm/dl)', 'Açúcar no sangue em jejum',
-       'Frequência cardíaca máxima alcançada', 'Oldpeak', 'Sexo_F', 'Sexo_M',
-       'Tipo de dor no peito_ASY', 'Tipo de dor no peito_ATA',
-       'Tipo de dor no peito_NAP', 'Tipo de dor no peito_TA',
-       'Resultados de eletrocardiograma em repouso_LVH',
-       'Resultados de eletrocardiograma em repouso_Normal',
-       'Resultados de eletrocardiograma em repouso_ST',
-       'Angina induzida por exercício_N', 'Angina induzida por exercício_Y',
-       'Inclinação do segmento ST de pico do exercício_Down',
-       'Inclinação do segmento ST de pico do exercício_Flat',
-       'Inclinação do segmento ST de pico do exercício_Up']
+columns=["Idade (anos)", "Pressão sanguínea em repouso (mm Hg)",
+       "Colesterol (mm/dl)", "Açúcar no sangue em jejum",
+       "Frequência cardíaca máxima alcançada", "Oldpeak", "Sexo_F", "Sexo_M",
+       "Tipo de dor no peito_ASY", "Tipo de dor no peito_ATA",
+       "Tipo de dor no peito_NAP", "Tipo de dor no peito_TA",
+       "Resultados de eletrocardiograma em repouso_LVH",
+       "Resultados de eletrocardiograma em repouso_Normal",
+       "Resultados de eletrocardiograma em repouso_ST",
+       "Angina induzida por exercício_N", "Angina induzida por exercício_Y",
+       "Inclinação do segmento ST de pico do exercício_Down",
+       "Inclinação do segmento ST de pico do exercício_Flat",
+       "Inclinação do segmento ST de pico do exercício_Up"]
 
 model = pickle.load(open('knn_model.sav', 'rb'))
 ro_scaler = pickle.load(open('ro_scaler.sav', 'rb'))
 
 app = app = Flask(__name__)
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 @app.route('/')
 def home():
@@ -27,6 +29,9 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
+
+    print(data)
+
     data_input = [data[col] for col in columns]
 
     df = pd.DataFrame(data=[data_input], columns=columns)
