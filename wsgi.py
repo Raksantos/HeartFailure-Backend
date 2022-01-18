5ac8927e735d5b52229e468bee3be3fe3f225e43
Raksantos <rodrigosnt3@gmail.com>
2022-01-18 18:09:31 -0300

    Setting enviroment variable port

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,6 @@
 from main import app
+import os
+
 if __name__ == "__main__":
-  app.run(debug=False, host="0.0.0.0")
+  port = os.enviroment.get("PORT", 5000)
+  app.run(debug=False, host="0.0.0.0", port=port)
