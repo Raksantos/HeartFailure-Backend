3e7a858c3bea6c5109a3afb7be3394587c340b50
Raksantos <rodrigosnt3@gmail.com>
2022-01-25 20:49:31 -0300

    Using cookiecutter

---
+++ datascience-backend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Backend desenvolvido para a disciplina de data science no IC-UFAL',
+    author='Rodrigo Santos da Silva e Gabriel Souza',
+    license='MIT',
+)
